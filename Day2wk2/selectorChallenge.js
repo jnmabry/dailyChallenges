75f1696c107fbacafa6ce21810f2880c6ed7a9a3
Joshua Mabry <JoshuaMabry@TestMBP.local>
2016-09-20 10:04:04 -0400

    Day 2 wk 2 Challenge

---
+++ Day2wk2/selectorChallenge.js
@@ -0,0 +1,12 @@
+$(function(){
+    
+    // Controls Style of ID paragraph
+    $("#paragraph").css("color","red");
+
+    // Controls all H3 elements
+    $("h3").css("color","blue");
+
+    // Control first li item in ul 
+    $('li').first().css("color", "red");
+
+});
